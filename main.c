e441b5be7fb2c74237f20f2fb8654f270dc146f4
Andrea Lambruschini <andrea.lambruschini@gmail.com>
2019-02-25 09:01:41 +0100

    Invio segnali RC divisi per 100

--- main.c
+++ main.c
@@ -165,14 +165,14 @@ int main(void)
             else if(rc_receiver_newData & RC_RECEIVER_TX_COMPLETE)
             {
                 if(rc_receiver_extract_Data(RC_BUFFER)) {
-                    pru0_data_struct->rc.throttle = RC_BUFFER[2];
-                    pru0_data_struct->rc.yaw      = RC_BUFFER[4];
-                    pru0_data_struct->rc.pitch    = RC_BUFFER[3];
-                    pru0_data_struct->rc.roll     = RC_BUFFER[1];
-                    pru0_data_struct->rc.aux1     = RC_BUFFER[6];
-                    pru0_data_struct->rc.aux2     = RC_BUFFER[5];
-                    pru0_data_struct->rc.aux3     = RC_BUFFER[7];
-                    pru0_data_struct->rc.aux4     = RC_BUFFER[8];
+                    pru0_data_struct->rc.throttle = RC_BUFFER[2]/100;
+                    pru0_data_struct->rc.yaw      = RC_BUFFER[4]/100;
+                    pru0_data_struct->rc.pitch    = RC_BUFFER[3]/100;
+                    pru0_data_struct->rc.roll     = RC_BUFFER[1]/100;
+                    pru0_data_struct->rc.aux1     = RC_BUFFER[6]/100;
+                    pru0_data_struct->rc.aux2     = RC_BUFFER[5]/100;
+                    pru0_data_struct->rc.aux3     = RC_BUFFER[7]/100;
+                    pru0_data_struct->rc.aux4     = RC_BUFFER[8]/100;
                     SEND_DATA_TO_P0(RC_DATA_MSG_TYPE);
                 }
             }
